55e62d06e7694fdec1e64b5dc815f639413a4973
Etch Akhtar <etch@clearcutcoding.com>
2018-05-05 17:18:59 +0100

    Create ES6 class - (doesn't build currently)

--- main.ts
+++ main.ts
@@ -1,5 +1,31 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+//import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//platformBrowserDynamic().bootstrapModule(AppModule);
+
+class ShoppingList {
+
+    groceries: string[];
+    
+    constructor() {
+        this.groceries = [];
+    }
+
+    addItem(item) {
+        this.groceries = [...this.groceries, item];
+    }
+
+    removeItem(item) {
+        this.groceries = this.groceries.filter(grocery => item !== grocery);
+    }
+}
+
+const list = new ShoppingList();
+
+list.addItem('Banana');
+list.addItem('Orange');
+console.log(list);
+
+list.removeItem('Orange');
+console.log(list);
