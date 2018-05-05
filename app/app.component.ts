5cc01ab09e2fb83eeb029f52a8f169b33f9b2e87
Etch Akhtar <etch@clearcutcoding.com>
2018-05-05 18:54:01 +0100

    initial

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title }}
+      <h1 [innerHTML]="title"></h1>
+      <h1>{{ title }}</h1>
+      <img [src]="logo">
+      <input type="text" [value]="name">
+      <div>{{ name }}</div>
     </div>
   `
 })
 export class AppComponent {
   title: string;
+  name: string = 'etch';
+  logo: string = 'img/logo.svg';
 
   constructor() {
     this.title = 'Ultimate Angular';
