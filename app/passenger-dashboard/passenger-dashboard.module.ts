c397ffeb48f247c74489f3c9c4ccd22c2717f206
Etch Akhtar <etch@clearcutcoding.com>
2018-05-06 11:20:42 +0100

    initial

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common'
 
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component'
 
+// containers
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component'
+
+// components
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component'
+
 @NgModule({
   declarations: [
-      PassengerDashboardComponent
+      PassengerDashboardComponent,
+      PassengerCountComponent,
+      PassengerDetailComponent
   ],
   imports: [
     CommonModule,
