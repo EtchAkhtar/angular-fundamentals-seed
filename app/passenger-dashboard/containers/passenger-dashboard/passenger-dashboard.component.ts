97ee8a896f31b67c9d0ec3954a973923b4d0c9f4
Etch Akhtar <etch@clearcutcoding.com>
2018-05-06 22:13:58 +0100

    8.9.ngSubmit

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -56,7 +56,6 @@ export class PassengerDashboardComponent implements OnInit {
           return passenger;
         });
       });
-
   }
 
 }
--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -10,7 +10,8 @@ import { Passenger } from '../../models/passenger.interface';
     template: `
       <div>
         <passenger-form
-          [passenger]="passenger">
+          [passenger]="passenger"
+          (update)="onUpdatePassenger($event)">
         </passenger-form>
       </div>
     `
@@ -25,5 +26,13 @@ export class PassengerViewerComponent implements OnInit {
         this.passengerService
         .getPassenger(1)
         .subscribe((data: Passenger) => this.passenger = data)
+    }
+
+    onUpdatePassenger(event: Passenger) {
+      this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passenger = Object.assign({}, this.passenger, event);
+      });
     }
   }
