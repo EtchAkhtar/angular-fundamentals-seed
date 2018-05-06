dfefe8c4af154371a8952e211077e8abb9b4a9c9
Etch Akhtar <etch@clearcutcoding.com>
2018-05-06 20:42:51 +0100

    initial

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 
-import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
-
 // containers
-import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
 
 // components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 
 // services
@@ -16,6 +16,7 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
 @NgModule({
   declarations: [
       PassengerDashboardComponent,
+      PassengerViewerComponent,
       PassengerCountComponent,
       PassengerDetailComponent
   ],
@@ -24,7 +25,8 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
     HttpModule
   ],
   exports: [
-      PassengerDashboardComponent
+      PassengerDashboardComponent,
+      PassengerViewerComponent
   ],
   providers: [
     PassengerDashboardService
